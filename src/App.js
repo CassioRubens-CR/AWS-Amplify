a8e64c8b1306e660ab49ae54fd1b163bbf1995d4
Cássio Rubens <cassiorubensadv@gmail.com>
2022-07-17 17:51:46 -0300

    FEAT: Added a GraphQL API and a database

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import { API } from 'aws-amplify';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
-import { listNotes } from './';
+import { listNotes } from './graphql/queries';
 import { createNote as createNoteMutation, deleteNote as deleteNoteMutation } from './graphql/mutations';
 
 const initialFormState = { name: '', description: '' }
